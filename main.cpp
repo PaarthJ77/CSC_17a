9d90a15e3cfd1ec96bf00feb7718e5ca6fea6fa2
PaarthJ77 <114961770+PaarthJ77@users.noreply.github.com>
2024-08-28 21:24:53 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,33 @@
+
+/*
+ * File:   main.cpp
+ * Author: Paarth Joshi
+ * Created on August 28, 2024, 9:47 PM
+ */
+
+// System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants - Mathematical, Scientific, Conversations
+//Higher Dimensions go here. No variables.
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Setting the random number seed
+
+    //Declaring Variables
+
+    //Initialize Variables
+
+    //Processing/Mapping Inputs to Outputs
+
+    //Displaying Input/Output Information
+    cout<<"Hello World"<<endl;
+    //Exiting stage left/right
+    return 0;
+}
